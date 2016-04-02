7c96a1fee6a46d044df75818012c32269d57d4ab
Duncan Mak <duncan.mak@xamarin.com>
2016-04-02 19:00:42 -0400

    Get the IDs from environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { createServer } from 'restify';
 import { BotConnectorBot } from 'botbuilder';
 
-const bot = new BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+const appId = process.env['BF_APP_ID'];
+const appSecret = process.env['BF_APP_SECRET'];
+const bot = new BotConnectorBot({ appId, appSecret });
 
 bot.add('/', (session) => session.send('Hello World'));
 
 const server = createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, () => console.log('%s listening to %s', server.name, server.url));
-
